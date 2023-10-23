15a9b3912837ea69488d7e862a54ac2231bf69e6
Alessandro1984 <bramucci.alessandro@gmail.com>
2023-10-23 17:34:10 +0200

    New exercises

---
+++ exercise2.R
@@ -0,0 +1,20 @@
+x <- 1:5
+
+x^2
+
+for (i in x) {
+  print("Hello")
+}
+
+x <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+for (i in x) {
+  print(paste0("Today is ", i))
+}
+
+name <- "Alessandro"
+
+paste0("My name is ", name)
+
+
+
---
+++ exercise3.R
@@ -0,0 +1,10 @@
+years <- 100
+A <- numeric(years)
+r <- 0.1
+P <- 1
+
+for (i in 1:years){
+  A[i] <- P * (1 + r)^i
+}
+
+P * (1 + r)^(1:years)
